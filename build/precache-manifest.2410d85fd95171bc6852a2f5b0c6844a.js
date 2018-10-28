d9de4ded164143dde502f57ed6130a4578c028c4
Sowmiya11 <sowmiyapriya1112@gmail.com>
2018-10-28 15:17:51 +0530

    Add files via upload

---
+++ build/precache-manifest.2410d85fd95171bc6852a2f5b0c6844a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21fd4c322bc4a1cc9add18b6a2015c17",
+    "url": "/Sowmiya11/dashboardpro/static/media/background.21fd4c32.jpg"
+  },
+  {
+    "revision": "f988335e64fe0319a9f7",
+    "url": "/Sowmiya11/dashboardpro/static/js/runtime~main.f988335e.js"
+  },
+  {
+    "revision": "34b4d86b68eff9a1c220",
+    "url": "/Sowmiya11/dashboardpro/static/js/main.34b4d86b.chunk.js"
+  },
+  {
+    "revision": "9ee170a6fbc953079056",
+    "url": "/Sowmiya11/dashboardpro/static/js/1.9ee170a6.chunk.js"
+  },
+  {
+    "revision": "34b4d86b68eff9a1c220",
+    "url": "/Sowmiya11/dashboardpro/static/css/main.9a7bf211.chunk.css"
+  },
+  {
+    "revision": "9a17f7d15761f9d95a541b1b0bd6cf2f",
+    "url": "/Sowmiya11/dashboardpro/index.html"
+  }
+];
